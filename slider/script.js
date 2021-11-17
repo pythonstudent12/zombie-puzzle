ab74a098090ef42b29d5271b37d31b4435d85062
1 <machinegun2@mail.ru>
2021-11-17 17:49:12 +0300

    first zombie commit

---
+++ slider/script.js
@@ -0,0 +1,103 @@
+/* Индекс слайда по умолчанию */
+ 
+var slides = document.getElementsByClassName("item");
+slides[0].style.display = "none";
+slides[1].style.display = "none";
+slides[2].style.display = "none";
+slides[3].style.display = "none";
+
+
+var again = document.getElementById("again");
+again.hidden = true;
+document.getElementsByClassName('header')[1].hidden = true;
+
+again.onclick = function() {
+
+    again.hidden = true;
+   
+    document.getElementById('hider').hidden = true;
+    document.getElementsByClassName('header')[1].hidden = true;
+
+
+    function randomInteger(min, max) {
+        // случайное число от min до (max+1)
+        let rand = min + Math.random() * (max + 1 - min);
+        return Math.floor(rand);
+      }
+    
+    var slideIndex = randomInteger(1,4);
+    console.log(slideIndex);
+    
+    
+    showSlides(slideIndex);
+    
+    
+    /* Основная функция сладера */
+    function showSlides(n) {
+    
+        var i;
+    
+    
+        //if (n > slides.length) {
+        //  slideIndex = 1;
+        //}
+       // if (n < 1) {
+       //     slideIndex = slides.length;
+       // }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+      
+        slides[n - 1].style.display = "block";
+
+    }
+    
+    setTimeout(() => {again.hidden = false;
+        document.getElementsByClassName('header')[1].hidden = false;
+        slides[0].style.display = "none";
+        slides[1].style.display = "none";
+        slides[2].style.display = "none"; 
+        slides[3].style.display = "none";
+        
+     }, 3000);
+}
+
+document.getElementsByClassName('main')[0].onclick = function() {
+    
+    document.getElementById('hider').hidden = true;
+    document.getElementsByClassName('header')[0].hidden = true;
+
+    function randomInteger(min, max) {
+        // случайное число от min до (max+1)
+        let rand = min + Math.random() * (max + 1 - min);
+        return Math.floor(rand);
+    }
+    
+    var slideIndex = randomInteger(1,4);
+           
+    showSlides(slideIndex);
+        
+    /* Основная функция сладера */
+    function showSlides(n) {
+      var i;
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      slides[n - 1].style.display = "block";
+    }
+    
+    setTimeout(() => {again.hidden = false;
+        document.getElementsByClassName('header')[1].hidden = false;
+        slides[0].style.display = "none";
+        slides[1].style.display = "none";
+        slides[2].style.display = "none"; 
+        slides[3].style.display = "none";
+        
+     }, 3000);
+
+    
+
+}
+
+//спасибо, что побывали в мире зомби
+//дописать функцию
